4702952f8b86e1026254f5c33927cbb14cbd9d77
PhillipDiCarlo <34153910+PhillipDiCarlo@users.noreply.github.com>
2022-08-11 18:53:31 -0400

    mongo integration and tmi integration

--- TwitchConnect.js
+++ TwitchConnect.js
@@ -1,41 +1,32 @@
 console.log("Twitch Bot Starting");
 
+//Requirements
 require("dotenv").config();
 //require("tedious");
 const tmi = require('tmi.js');
-var mysql = require("mysql");
+var HashMap = require("hashmap");
 
-//mysql connection
-var sqlServ = mysql.createConnection({
-    host: process.env.SQL_HOST,
-	port: process.env.SQL_PORT,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PASSWD,
-	insecureAuth: true
-})
+//MongoDB connection
+var MongoClient = require('mongodb').MongoClient;
+var databaseName = "test";
+var collectionName = "streamers";
 
-sqlServ.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected to SQL Server!");
-});
+var italiandogsWorth = 45;
+var mzikifWorth = 0;
+var netWorthMap = new HashMap();
 
-
-// function wait(ms)
-// {
-//     var d = new Date();
-//     var d2 = null;
-//     do { d2 = new Date(); }
-//     while(d2-d < ms);
-// }
-
-// function randomIntFromInterval(min, max) { // min and max included 
-// 	x = Math.floor(Math.random() * (max - min + 1) + min)
-// 	return x * 1000
-
-//   }
+//Networth formula: in env file
+netWorthMap.set("mizkif", 231731);
+netWorthMap.set("italiandogs", 22);
 
 
-const client = new tmi.Client({
+async function insertCorpa(client, newInsert) {
+	const result = await client.db(databaseName).collection(collectionName).insertOne(newInsert);
+	//console.log("document inserted")
+}
+
+
+const twitchClient = new tmi.Client({
 	options: { debug: true },
 	identity: {
 		username: process.env.TWITCH_BOT_USERNAME,
@@ -45,20 +36,47 @@ const client = new tmi.Client({
 });
 
 
-client.connect();
+twitchClient.connect();
 
-client.on('message', (channel, tags, message, self) => {
+twitchClient.on('message', (channel, tags, message, self) => {
 	// Ignore echoed messages.
 	if(self) return;
 
-    //tell chat gn
-	if(message.toLowerCase() === 'corpa sell') {
-		client.say(channel, 'corpa HOT SALE!!!!');
+    //Sell Stonks
+	if(message.toLowerCase() === 'corpa sell' ||
+	   message.toLowerCase() === 'sell corpa') {
+		netWorthMap.set(channel.toString().substring(1), netWorthMap.get(channel.toString().substring(1))-0.01);
+		insertCorpa(new MongoClient(process.env.MONGO_URL.concat(channel.toString().substring(1))), {
+			channelId: channel,
+			stockName: "$".concat(channel.toString().substring(1)),
+			stockPrice: netWorthMap.get(channel.toString().substring(1)),
+			timestamp: new Date().toISOString()
+		})
 	 }
 
-    //i miss ockie
-    if(message.toLowerCase() === "corpa buy") {
-		client.say(channel, `corpa BUY NOW!!!!`);
+    //Buy Stonks
+    if(message.toLowerCase() === "corpa buy" ||
+	   message.toLowerCase() === 'buy corpa') {
+		netWorthMap.set(channel.toString().substring(1), netWorthMap.get(channel.toString().substring(1))+0.01);
+		console.log (netWorthMap.get("italiandogs"));
+		insertCorpa(new MongoClient(process.env.MONGO_URL.concat(channel.toString().substring(1))), {
+			channelId: channel,
+			stockName: "$".concat(channel.toString().substring(1)),
+			stockPrice: netWorthMap.get(channel.toString().substring(1)),
+			timestamp: new Date().toISOString()
+		})
+	 }
+
+	 //Check Streamer Value
+	 if(message.toLowerCase() === "!corpastonk") {
+		twitchClient.say(channel, `@${tags.username} $${channel.toString().substring(1).toUpperCase()} is worth bubcus`);
+
+
+	 }
+
+	 //debugger
+	 if(message === "!dbg"){
+		console.log(new MongoClient(process.env.MONGO_URL.concat(channel.toString().substring(1))).db(databaseName).collection(collectionName).find().sort({_id:-1}).limit(1));
 	 }
 
 });
