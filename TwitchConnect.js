855fab3cdb3542d8610d31990bf1388874370d6a
PhillipDiCarlo <34153910+PhillipDiCarlo@users.noreply.github.com>
2022-08-12 02:05:03 -0400

    Additional streamers now easily be added

--- TwitchConnect.js
+++ TwitchConnect.js
@@ -3,80 +3,156 @@ console.log("Twitch Bot Starting");
 //Requirements
 require("dotenv").config();
 //require("tedious");
-const tmi = require('tmi.js');
+const tmi = require("tmi.js");
 var HashMap = require("hashmap");
 
 //MongoDB connection
-var MongoClient = require('mongodb').MongoClient;
-var databaseName = "test";
-var collectionName = "streamers";
-
-var italiandogsWorth = 45;
-var mzikifWorth = 0;
+var MongoClient = require("mongodb").MongoClient;
+var databaseName = "streamerStocks";
+var collectionName; // TODO
 var netWorthMap = new HashMap();
 
 //Networth formula: in env file
-netWorthMap.set("mizkif", 231731);
-netWorthMap.set("italiandogs", 22);
 
+//initial set of streamers worth if collection exists
+async function setNetworth(streamer) {
+  var clientName = new MongoClient(process.env.MONGO_URL.concat(streamer));
+  await getCorpa(clientName, streamer).then((tempIPO) => {
+    netWorthMap.set(streamer, tempIPO);
+  });
 
-async function insertCorpa(client, newInsert) {
-	const result = await client.db(databaseName).collection(collectionName).insertOne(newInsert);
-	//console.log("document inserted")
+  //console.log(netWorthMap.get("italiandogs"));
 }
 
+async function insertCorpa(client, streamerName, newInsert) {
+  const result = await client
+    .db(databaseName)
+    .collection(streamerName)
+    .insertOne(newInsert);
+  console.log("document inserted");
+}
+
+//get stonk price
+async function getCorpa(client, channelName) {
+  client.connect();
+  const db = client.db(databaseName);
+  const collection = db.collection(channelName);
+  const cursor = collection.find().sort("timestamp", -1).limit(1);
+  const item = await cursor.next();
+  const finalItem = item.stockPrice;
+  return finalItem;
+}
 
 const twitchClient = new tmi.Client({
-	options: { debug: true },
-	identity: {
-		username: process.env.TWITCH_BOT_USERNAME,
-		password: process.env.TWITCH_OAUTH_TOKEN
-	},
-	channels: [ 'Italiandogs']
+  options: { debug: true },
+  identity: {
+    username: process.env.TWITCH_BOT_USERNAME,
+    password: process.env.TWITCH_OAUTH_TOKEN,
+  },
+  // Channels to watch
+  channels: ["Italiandogs"],
 });
-
 
 twitchClient.connect();
 
-twitchClient.on('message', (channel, tags, message, self) => {
-	// Ignore echoed messages.
-	if(self) return;
+twitchClient.on("message", (channel, tags, message, self) => {
+  // Ignore echoed messages.
+  if (self) return;
 
-    //Sell Stonks
-	if(message.toLowerCase() === 'corpa sell' ||
-	   message.toLowerCase() === 'sell corpa') {
-		netWorthMap.set(channel.toString().substring(1), netWorthMap.get(channel.toString().substring(1))-0.01);
-		insertCorpa(new MongoClient(process.env.MONGO_URL.concat(channel.toString().substring(1))), {
-			channelId: channel,
-			stockName: "$".concat(channel.toString().substring(1)),
-			stockPrice: netWorthMap.get(channel.toString().substring(1)),
-			timestamp: new Date().toISOString()
-		})
-	 }
+  //Sell Stonks
+  if (
+    message.toLowerCase() === "corpa sell" ||
+    message.toLowerCase() === "sell corpa"
+  ) {
+    var streamerName = channel.toString().toLowerCase().substring(1);
+    netWorthMap.set(
+      channel.toString().substring(1),
+      netWorthMap.get(channel.toString().substring(1)) - 0.01
+    );
+    insertCorpa(
+      new MongoClient(
+        process.env.MONGO_URL.concat(channel.toString().substring(1))
+      ),
+      streamerName,
+      {
+        channelId: channel,
+        stockName: "$".concat(channel.toString().substring(1)),
+        stockPrice: netWorthMap.get(channel.toString().substring(1)),
+        timestamp: new Date().toISOString(),
+      }
+    );
+  }
 
-    //Buy Stonks
-    if(message.toLowerCase() === "corpa buy" ||
-	   message.toLowerCase() === 'buy corpa') {
-		netWorthMap.set(channel.toString().substring(1), netWorthMap.get(channel.toString().substring(1))+0.01);
-		console.log (netWorthMap.get("italiandogs"));
-		insertCorpa(new MongoClient(process.env.MONGO_URL.concat(channel.toString().substring(1))), {
-			channelId: channel,
-			stockName: "$".concat(channel.toString().substring(1)),
-			stockPrice: netWorthMap.get(channel.toString().substring(1)),
-			timestamp: new Date().toISOString()
-		})
-	 }
+  //Buy Stonks
+  if (
+    message.toLowerCase() === "corpa buy" ||
+    message.toLowerCase() === "buy corpa"
+  ) {
+    var streamerName = channel.toString().toLowerCase().substring(1);
+    netWorthMap.set(
+      channel.toString().substring(1),
+      netWorthMap.get(channel.toString().substring(1)) + 0.01
+    );
+    insertCorpa(
+      new MongoClient(
+        process.env.MONGO_URL.concat(channel.toString().substring(1))
+      ),
+      streamerName,
+      {
+        channelId: channel,
+        stockName: "$".concat(channel.toString().substring(1)),
+        stockPrice: netWorthMap.get(channel.toString().substring(1)),
+        timestamp: new Date().toISOString(),
+      }
+    );
+  }
 
-	 //Check Streamer Value
-	 if(message.toLowerCase() === "!corpastonk") {
-		twitchClient.say(channel, `@${tags.username} $${channel.toString().substring(1).toUpperCase()} is worth bubcus`);
+  //Check Streamer Value
+  if (message.toLowerCase() === "!corpastonk") {
+    var clientName = new MongoClient(
+      process.env.MONGO_URL.concat(channel.toString().substring(1))
+    );
+    var channelName = channel.toString().substring(1);
+    getCorpa(clientName, channelName).then((corpaCost) => {
+      twitchClient.say(
+        channel,
+        `@${tags.username} $${channel
+          .toString()
+          .substring(1)
+          .toUpperCase()} is worth ${corpaCost.toFixed(2)}`
+      );
+    });
+  }
 
+  //debugger
+  if (message === "!dbg") {
+    var streamerName = channel.toString().toLowerCase().substring(1);
+    netWorthMap.set(
+      channel.toString().substring(1),
+      netWorthMap.get(channel.toString().substring(1)) + 0.01
+    );
 
-	 }
-
-	 //debugger
-	 if(message === "!dbg"){
-		console.log(new MongoClient(process.env.MONGO_URL.concat(channel.toString().substring(1))).db(databaseName).collection(collectionName).find().sort({_id:-1}).limit(1));
-	 }
-
+    insertCorpa(
+      new MongoClient(
+        process.env.MONGO_URL.concat(channel.toString().substring(1))
+      ),
+      streamerName,
+      {
+        channelId: channel,
+        stockName: "$".concat(channel.toString().substring(1)),
+        stockPrice: netWorthMap.get(channel.toString().substring(1)),
+        timestamp: new Date().toISOString(),
+      }
+    );
+  }
 });
+
+async function main() {
+  netWorthMap.set("mizkif", 231731);
+  netWorthMap.set("italiandogs", 22);
+
+  // await setNetworth("italiandogs");
+  // console.log(netWorthMap.get("italiandogs"));
+}
+
+main();
